d045cbf8dd9b4e571ba9254545684c0ce2267313
ArtPoon <artpoon@gmail.com>
2024-03-06 15:23:29 -0500

    start code analysis, update style

--- R/estimate.dynsbm.R
+++ R/estimate.dynsbm.R
@@ -12,47 +12,67 @@
 
 # You should have received a copy of the GNU General Public License
 # along with dynsbm.  If not, see <http://www.gnu.org/licenses/>
-		
-estimate.dynsbm <- function (Y, present=NULL, Q,
-                             edge.type=c("binary","discrete","continuous"), K=-1,
-                             directed=FALSE, self.loop=FALSE,
-                             init.cluster=NULL,
-                             nb.cores=1,
-                             perturbation.rate=0., iter.max=20,
-                             fixed.param=FALSE,
-                             bipartition=NULL) {
-    T <- dim(Y)[1]
-    N <- dim(Y)[2]
+
+#' @param Y:  A T x N x N matrix representing a time series of NxN adjacency 
+#'            matrices at T time points, where N is the number of nodes in the 
+#'            network.
+#' @param present:  optional T x N matrix representing the presence/absence
+#'                  of N nodes at each time point, i.e., missing data.
+#' @param Q:  integer, number of groups
+#' @param edge.type:  character, type of adjacency matrix
+#' @param K:  integer, number of non-zero discrete values (if edge.type is discrete)
+#' @param directed:  logical, if TRUE then network has directed edges
+#' @param self.loop:  logical, if TRUE, permit self-loops (non-zero diagonal in 
+#'                    adjacency matrix)
+#' @param init.cluster:  
+#' @param nb.cores:  integer, number of cores to run parallel::mclapply
+#' @param perturbation.rate:  numeric, fraction of nodes to reassign to groups
+#' @param iter.max:  integer, maximum number of iterations for algorithm
+#' @param fixed.param:  logical, if TRUE then model parameters are fixed.
+#' @param bipartition:  optional integer vector of length N assigning N nodes to 
+#'                      partition 1 or 2.
+estimate.dynsbm <- function(
+    Y, present=NULL, Q, edge.type=c("binary","discrete","continuous"), K=-1,
+    directed=FALSE, self.loop=FALSE, init.cluster=NULL, nb.cores=1,
+    perturbation.rate=0., iter.max=20, fixed.param=FALSE, bipartition=NULL) 
+  {
+    # handle input parameters
+    n.times <- dim(Y)[1]  # number of time points
+    n.nodes <- dim(Y)[2]  # number of nodes
+    
     if (is.null(present)){
-        present <- matrix(1L,dim(Y)[2],dim(Y)[1])
-    } else{
+      # declare a NxT matrix of integer 1's
+      present <- matrix(1L, n.nodes, n.times) #dim(Y)[2], dim(Y)[1])
+    } else {
         if (storage.mode(present)!= "integer") storage.mode(present) <- "integer"
     }
-    is.bipartite = FALSE
-    if(!is.null(bipartition))
-        is.bipartite = TRUE
+    is.bipartite <- !is.null(bipartition)
+    
     ##---- initialization
-    if(is.null(init.cluster)){
+    if (is.null(init.cluster)) {
         ## aggregation
         Yaggr <- c()
-        if(!any(present==0L)){ ## concat all time step if no absent nodes
-            for (t in 1:T){
-                if(directed){
-                    Yaggr <- cbind(Yaggr,Y[t,,],t(Y[t,,]))
-                } else{
-                    Yaggr <- cbind(Yaggr,Y[t,,])
-                }
+        if (!any(present==0L)) {
+          # concat all time step if no absent nodes
+          for (tp in 1:n.times) {
+            if (directed) {
+              Yaggr <- cbind(Yaggr, Y[tp,,], t(Y[tp,,]))
+            } else {
+              Yaggr <- cbind(Yaggr, Y[tp,,])
             }
-        } else{ ## aggregate all time step if absent nodes
-            Yaggr <- Y[1,,]
-            for (t in 2:T){
-                Yaggr <- Yaggr+Y[t,,]
-            }
-            nbcopresent <- present %*% t(present)
-            nbcopresent[nbcopresent==0] <- -1 # to avoid NaN
-            Yaggr <- Yaggr / nbcopresent
-            if(directed) Yaggr <- cbind(Yaggr,t(Yaggr))
+          }
+        } else {
+          # aggregate all time step if absent nodes
+          Yaggr <- Y[1,,]
+          for (tp in 2:n.times){
+            Yaggr <- Yaggr + Y[tp,,]
+          }
+          nbcopresent <- present %*% t(present)
+          nbcopresent[nbcopresent==0] <- -1 # to avoid NaN
+          Yaggr <- Yaggr / nbcopresent
+          if(directed) Yaggr <- cbind(Yaggr,t(Yaggr))
         }
+        
         if(is.bipartite){ 
             ## bipartite case       
             set1 <- which(bipartition==1)
@@ -100,7 +120,7 @@ estimate.dynsbm <- function (Y, present=NULL, Q,
             }
             ## perturbation
             if(perturbation.rate>0.){
-                v <- sample(1:N, perturbation.rate*N)
+                v <- sample(1:n.nodes, perturbation.rate*n.nodes)
                 vs <- sample(v)
                 init.cluster[v] <- init.cluster[vs]
             }
@@ -109,7 +129,7 @@ estimate.dynsbm <- function (Y, present=NULL, Q,
     ##---- estimation
     result <- list(
         init.cluster=init.cluster,
-        dynsbm=dynsbmcore(T, N, Q, as.vector(Y), present, edge.type, K, init.cluster-1, iter.max, nb.cores, directed, self.loop, fixed.param) # -1 for compatibility with C++   
+        dynsbm=dynsbmcore(n.times, n.nodes, Q, as.vector(Y), present, edge.type, K, init.cluster-1, iter.max, nb.cores, directed, self.loop, fixed.param) # -1 for compatibility with C++   
     )
     ##---- verifying bipartition
     if(is.bipartite){ 
@@ -121,7 +141,7 @@ estimate.dynsbm <- function (Y, present=NULL, Q,
     }
     
     ##---- correcting trans in the case of classical SBM
-    if(T==1){
+    if(n.times==1){
         result$dynsbm$trans <- matrix(0,Q,Q)
         diag(result$dynsbm$trans) <- 1
     }
